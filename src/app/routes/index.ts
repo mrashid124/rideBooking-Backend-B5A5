1d3430c8e1acd1a08b11c2ea5e3ed6cd4de4cfd6
mrashid124 <mrashidraju@gmail.com>
2025-08-02 12:10:42 +0100

    Rider booking routes  updated

---
+++ src/app/routes/index.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+// import { AuthRoutes } from "../modules/auth/auth.route";
+// import { DriverRoutes } from "../modules/driver/driver.route";
+// import { RideRoutes } from "../modules/ride/ride.route";
+import { UserRoutes } from "../modules/user/user.route";
+
+export const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/users",
+    route: UserRoutes,
+  },
+//   {
+//     path: "/auth",
+//     route: AuthRoutes,
+//   },
+//   {
+//     path: "/rides",
+//     route: RideRoutes,
+//   },
+//   {
+//     path: "/drivers",
+//     route: DriverRoutes,
+//   },
+];
+
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
